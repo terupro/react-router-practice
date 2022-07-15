efcdf07a7c690bf789b35e8789789c5395a0f0e7
‘terupro’ <terupro911@gmail.com>
2022-07-15 18:17:39 +0900

    stateを渡す画面遷移

--- src/components/Page1DetailA.jsx
+++ src/components/Page1DetailA.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export const Page1DetailA = () => {
+   const { state } = useLocation();
+   console.log(state);
   return (
     <div>
       <h1>Page1 DetailA</h1>
