fe595f41ed1d1f2db635c719b926650866593b7c
‘terupro’ <terupro911@gmail.com>
2022-07-15 18:35:18 +0900

    404

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -4,6 +4,7 @@ import { Page1 } from "../components/Page1"
 import { Page1DetailA } from "../components/Page1DetailA"
 import { Page1DetailB } from "../components/Page1DetailB"
 import { Page2 } from "../components/Page2"
+import { Page404 } from "../components/Page404"
 import { UrlParameter } from "../UrlParameter"
 
 
@@ -16,6 +17,8 @@ export const Routing = () => {
       <Route path='/page1/detailB' element={<Page1DetailB />}></Route>
     <Route path='/page2' element={<Page2 />}></Route>
      <Route path='/page2/:id' element={<UrlParameter />}></Route>
+     <Route path="*" element={<Page404/>}></Route>
    </Routes>
+   
   )
 }
